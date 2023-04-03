b858e8dde940273c66d3fc2ca79973565dc74277
kenneth suarez <33601506+polopopeye@users.noreply.github.com>
2023-04-03 23:51:50 +0200

    delete redux :'(

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
 import RootLayout from '@/app/components/layout';
-// import HooksContainer from '@/app/hooks/hooksContainer';
-// import store from '@/app/store/store';
-// import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }: any) {
   return (
     <RootLayout>
-      {/* <Provider 
-        store={store}
-        > */}
       <Component {...pageProps} />
-      {/* <HooksContainer /> */}
-      {/* </Provider> */}
     </RootLayout>
   );
 }
